eea2c6ff8593cac2ba9a223e573b858ba4220a17
Helamã <cristian.pinheiro>
2023-08-16 17:18:40 -0300

    P1 done

--- ts/src/p1.ts
+++ ts/src/p1.ts
@@ -1,11 +1,52 @@
 export function isPalindrome(s: string): boolean {
-    return false;
+
+
+    // const reverse = s.split('').reverse().join('');
+    let reverse = "";
+    for (let i = s.length - 1; i >= 0; i--) {
+        reverse += s[i];
+    }
+
+    if (reverse.toLocaleLowerCase() === s.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
 
 export function reverseWordsInString(s: string): string {
-    return s;
+
+    let reverse = "";
+    let words = s.split(' ');
+    for (let i = words.length - 1; i >= 0; i--) {
+        reverse += words[i] + " ";
+    }
+
+    return reverse.trim()
+
 }
 
 export function doCalculation(s: string): number {
-    return 0;
+   
+    let numbers = s.split(/[^0-9\.]+/);
+    let operators = s.split(/[0-9\.]+/);
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (i === 0) {
+            result = Number(numbers[i]);
+        }
+        else {
+            if (operators[i] === "+") {
+
+                result += Number(numbers[i]);
+            }
+            else if (operators[i] === "-") {
+                result -= Number(numbers[i]);
+            }
+        }
+    }
+    return result;
+
 }
